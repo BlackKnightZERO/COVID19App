815e09ba65326dd8e84e3a6b4cb4a92e2acb3ff7
Arif Faysal <ayon.arif.10@gmail.com>
2020-04-14 20:51:06 +0600

    chartJs added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AppService } from '../app.service';
+import { LoadingController } from '@ionic/angular';
+import { Chart } from 'chart.js';
+import { ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-tab1',
@@ -9,20 +12,106 @@ import { AppService } from '../app.service';
 
 export class Tab1Page {
 
+  @ViewChild('barChart', null) barChart: any;
+  @ViewChild('horizontalBar', null) horizontalBarChart: any;
+  @ViewChild('line2', null) line2Chart: any;
+
+  bars: any;
+  horizontalBar: any;
+  line2: any;
+  colorArray: any;
+  data: any;
+  isSaving = false;
+
+
   global:any;
-  constructor(public appService: AppService) {}
+  constructor(
+    public appService: AppService, 
+    public loadingController:LoadingController,
+    ){}
   
   ionViewDidEnter(){
     this.getData();
   }
   
-  getData(){
-    this.appService.getData().subscribe((res)=> {
-      this.global = res['Global']
-      //console.log(this.global);
+  async getData(){
+    const loading = await this.loadingController.create({
+      message: 'Loading...',
+      duration: 15000
+    });
+    await loading.present();
+
+    // const { role, data } = await loading.onDidDismiss();
+    // console.log('Loading dismissed!');
+
+      this.appService.getData().subscribe((res)=> {
+      this.global = res['Global'];
+      this.createBarChart();
+      this.createHorizontalBarChart();
+      loading.dismiss();
     },
     (error) => {
 
     });
   }
+
+  createBarChart() {
+    const newPerc = Math.floor((this.global.NewConfirmed / this.global.TotalConfirmed) * 100);
+    const recoverdPerc = Math.floor((this.global.TotalRecovered / this.global.TotalConfirmed) * 100);
+    const deathPerc = Math.floor((this.global.TotalDeaths / this.global.TotalConfirmed) * 100);
+    const activePerc = 100 - (recoverdPerc + deathPerc);
+    
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      height:400,
+      data: {
+        labels: ['Active(' + activePerc + '%)', 'Recoverd(' + recoverdPerc + '%)', 'Deaths (' + deathPerc + '%)', 'New(' + newPerc + '%)'],
+        datasets: [{
+          label: 'Total Percentage value',
+          data: [activePerc, recoverdPerc, deathPerc, newPerc],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  createHorizontalBarChart() {
+    this.horizontalBar = new Chart(this.horizontalBarChart.nativeElement, {
+      type: 'horizontalBar',
+      data: {
+        labels: ['Cases', 'Recovered', 'Deaths'],
+        datasets: [{
+          label: 'Total',
+          data: [this.global.TotalConfirmed, this.global.TotalRecovered, this.global.TotalDeaths],
+          backgroundColor: '#fca330', // array should have same number of elements as number of dataset
+          borderColor: '#fca330',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        },
+        {
+          label: 'New',
+          data: [this.global.NewConfirmed, this.global.NewRecovered, this.global.NewDeaths],
+          backgroundColor: '#fc2205', // array should have same number of elements as number of dataset
+          borderColor: '#fc2205',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+    });
+  }
+
+
 }
+
+
+
+
